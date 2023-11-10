e961d8a29e9dc9d5142949d94a80b6deb83db127
Ward Wheeler <ward@system76-pc.localdomain>
2023-11-10 17:04:24 -0500

    Added readme maybe in graph conversion

--- pkg/PHANE-PhyloLib/src/GraphFormatUtilities.hs
+++ pkg/PHANE-PhyloLib/src/GraphFormatUtilities.hs
@@ -152,6 +152,7 @@ import Data.Maybe
 import Data.Monoid
 import Data.Text qualified as StrictT
 import Data.Text.Lazy qualified as T
+import Text.Read
 import GeneralUtilities
 import ParallelUtilities
 
@@ -1034,7 +1035,11 @@ stringGraph2TextGraphDouble inStringGraph =
     in  G.mkGraph newNodes newEdges
     where
         dummyRelabelEdges ∷ (a, b, String) → (a, b, Double)
-        dummyRelabelEdges (a, b, c) = (a, b, read c ∷ Double)
+        dummyRelabelEdges (a, b, c) = 
+                let newC = readMaybe c ∷ Maybe Double
+                in
+                if isJust newC then (a, b, fromJust newC)
+                else (a, b, 0.0)
 
 
 -- |  textGraph2StringGraph take P.Gr String a and converts to P.Gr Text a
